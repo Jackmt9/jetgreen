e285ca06a7abaf4bc7ccc7263f7f075483d48559
Jackmt9 <jackmt9@gmail.com>
2020-04-10 19:15:45 -0400

    Macros

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :passenger
   belongs_to :plane
+  has_many :mfs
+  has_many :ffs
+  has_many :movies, through: :mfs
+  has_many :foods, through: :ffs
 end
